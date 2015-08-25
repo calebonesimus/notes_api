0c08057d6ac36c433a91fcfc344a5e7e4dd93358
Caleb Francis <calebonesimus@gmail.com>
2015-08-25 17:15:01 -0400

    API shows notes with a given tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :notes, except: [:new, :edit]
+  
   get '/api/notes' => 'notes#index'
   post '/api/notes' => 'notes#create'
+
   get '/api/notes/tag/:tag_name' => 'tags#index'
 end
