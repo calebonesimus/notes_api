e9c2e8f5f8eb960b785e2885c70bdf024ddefdb5
Caleb Francis <calebonesimus@gmail.com>
2015-08-25 13:58:22 -0400

    Notes can be added and viewed with json requests

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,24 @@
 class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+    render json: @notes.to_json
+  end
+
+  def create
+    @note = Note.create(note_params)
+    if @note.save
+      render json: @note.to_json
+    else
+      render json: @note.errors
+    end
+  end
+
+  private
+
+  def note_params
+    params.permit(:body, :title)
+  end
+
+
 end
