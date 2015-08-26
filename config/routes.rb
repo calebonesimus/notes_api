ef916ced5199b523ef9b011b320814c193b09273
Caleb Francis <calebonesimus@gmail.com>
2015-08-26 16:28:02 -0400

    Create api namespace for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :comments, except: [:new, :edit]
-  # resources :users, except: [:new, :edit]
-  # resources :notes, except: [:new, :edit]
+  namespace :api do
+    resources :users, except: [:new, :edit]
+    resources :notes, except: [:new, :edit] do
+      resources :comments, except: [:new, :edit]
+    end
+  end
 
   post '/api/users' => 'users#create'
 
-  get '/api/notes' => 'notes#index'
-  post '/api/notes' => 'notes#create'
-  get '/api/notes/:id' => 'notes#show'
-  patch '/api/notes/:id' => 'notes#update'
+  # get '/api/notes' => 'notes#index'
+  # post '/api/notes' => 'notes#create'
+  # get '/api/notes/:id' => 'notes#show'
+  # patch '/api/notes/:id' => 'notes#update'
+
 
   get '/api/notes/tag/:tag_name' => 'notes#by_tag'
 end
