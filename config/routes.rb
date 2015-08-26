795b7bcf1769f68d210b04d65143b5fe941da981
Caleb Francis <calebonesimus@gmail.com>
2015-08-26 16:36:46 -0400

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, except: [:new, :edit]
-    resources :notes, except: [:new, :edit] do
-      resources :comments, except: [:new, :edit]
+    resources :users, only: [:create]
+    resources :notes, only: [:index, :show, :create, :update] do
+      resources :comments, only: [:create]
     end
   end
-
-  post '/api/users' => 'api/users#create'
-
-  # get '/api/notes' => 'notes#index'
-  # post '/api/notes' => 'notes#create'
-  # get '/api/notes/:id' => 'notes#show'
-  # patch '/api/notes/:id' => 'notes#update'
-
 
   get '/api/notes/tag/:tag_name' => 'api/notes#by_tag'
 end
